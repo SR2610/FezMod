601dff2d6b563d7786ad8b763fd2ebecf3ab2426
Stephen Ray <SR2610Email@Gmail.com>
2016-05-29 01:39:13 +0100

    Added model for fez
    Also added texture, both need to be changed before release.

---
+++ src/main/java/com/sr2610/fezmod/client/model/ModelFez.java
@@ -0,0 +1,41 @@
+package com.sr2610.fezmod.client.model;
+
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+
+public class ModelFez extends ModelBiped {
+
+	public ModelFez() {
+		textureWidth = 64;
+		textureHeight = 32;
+
+		bipedHead = new ModelRenderer(this, 34, 0);
+		bipedHead.addBox(-2F, -10F, -2F, 4, 3, 4);
+		setRotation(bipedHead, 0f, 0F, -0.1F);
+
+		bipedHead.setTextureOffset(51, 0);
+		bipedHead.addBox(-3F, -11F, 0F, 3, 1, 1);
+		bipedHead.setTextureOffset(51, 0);
+		bipedHead.addBox(-3F, -10F, 0F, 1, 2, 1);
+
+	}
+
+	@Override
+	public void render(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+		setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entity);
+		GL11.glPushMatrix();
+		GL11.glScaled(1.2, 1.2, 1.2);
+		bipedHead.render(scale);
+		GL11.glPopMatrix();
+	}
+
+	private void setRotation(ModelRenderer model, float x, float y, float z) {
+		model.rotateAngleX = x;
+		model.rotateAngleY = y;
+		model.rotateAngleZ = z;
+	}
+
+}
--- src/main/java/com/sr2610/fezmod/items/ItemFez.java
+++ src/main/java/com/sr2610/fezmod/items/ItemFez.java
@@ -1,25 +1,47 @@
 package com.sr2610.fezmod.items;
 
 import com.sr2610.fezmod.Reference;
+import com.sr2610.fezmod.client.model.ModelFez;
 
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemFez extends ItemModArmor{
+public class ItemFez extends ItemModArmor {
 
 	public ItemFez(ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn,
 			String unlocalizedName) {
 		super(materialIn, renderIndexIn, equipmentSlotIn, unlocalizedName);
 	}
-	
+
 	public ItemFez(String unlocalizedName) {
 		super(ArmorMaterial.LEATHER, 0, EntityEquipmentSlot.HEAD, unlocalizedName);
 	}
-	
+
 	@Override
 	public String getArmorTexture(ItemStack stack, Entity entity, EntityEquipmentSlot slot, String type) {
-		return Reference.MOD_ID + ":textures/models/armor/fez.png";
+		return Reference.MOD_ID + ":textures/models/fez.png";
 	}
+	
+    @SideOnly(Side.CLIENT)
+    private ModelFez model = new ModelFez();
+	
+	@Override
+    @SideOnly(Side.CLIENT)
+	public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot, net.minecraft.client.model.ModelBiped _default)
+    {
+        return model;
+    }
+
+
+
+
+
+       
 
 }
